0860c4e4a4e50ef8ce8a3a43f1d244fca29441b8
Tchollo88 <Tcholloman@gmail.com>
2024-10-27 15:58:18 -0700

    Add project files.

---
+++ Holloman_SortingAlgorithm_HW/Program.cs
@@ -0,0 +1,208 @@
+﻿using Holloman_SortingAlgorithm_HW;
+using System.Diagnostics;
+
+/*I have been running accross an interesting bug, You can run the Quick Sort Algorithm if you comment out all other algorithms. When you try running them back to back, like i originally set up, it goes through a stacxk overflow, I am still working out why this is happening.*/
+
+Stopwatch stopwatch = Stopwatch.StartNew();
+
+#region ** Building Randomized Array **
+// usage 100 thousand values
+Console.WriteLine("Building randomized array.");
+
+stopwatch.Start();
+
+int[] largeArr = GenerateRandomArray(100000, 1, 1000);
+Console.WriteLine("Array built.");
+
+stopwatch.Stop();
+DisplayRuntime(stopwatch);
+#endregion
+
+#region ** Duplication of Randomized Array **
+int[] Bubble, Insert, Mg, Quick, Reset;
+Bubble = largeArr;
+Insert = largeArr;
+Mg = largeArr;
+Quick = largeArr;
+Reset = largeArr;
+
+int[] result = largeArr;
+#endregion
+
+#region ** Run Bubble Algorithm **
+//stopwatch.Reset();
+
+//Console.WriteLine("\nBubble Sorting Algorithm: ");
+//Console.WriteLine("Starting the sort.");
+
+//stopwatch.Start();
+
+//result = Sort.BubbleSort(Bubble, Bubble.Length);
+//Console.WriteLine("Finished sorting!");
+
+//stopwatch.Stop();
+//DisplayRuntime(stopwatch);
+
+//Console.WriteLine("Press enter to continue.");
+//Console.ReadLine();
+/**------------------------------------Stop Here-----------------------------------------**/
+
+/*
+
+Reasoning: I believe that the reason Bubble Sort's performance lags behind and becomes the 
+slowest in this list of algorithms is because it the longer the array needing to be sorted gets
+it's processing spped for the data falls into the [O(n^2)]. This means that it needs to compile
+and compare twice as meany times compared to the processing of [O(nlogn)] or even the best case
+scenario of [O(n^1)].
+
+ */
+
+// ----------------------------------------------------- Array Test
+//ArrayCheck(result);
+
+/**Other*/
+result = Reset;
+#endregion
+
+#region ** Run Insertion Algorithm **
+/** Insertion Sort Opperation **/
+//stopwatch.Reset();
+
+//Console.WriteLine("\nInsertion Sorting Algorithm: ");
+//Console.WriteLine("Starting the sort.");
+
+//stopwatch.Start();
+
+//result = Sort.InsertionSort(Insert, Insert.Length);
+//Console.WriteLine("Finished sorting!");
+
+//stopwatch.Stop();
+//DisplayRuntime(stopwatch);
+
+//Console.WriteLine("Press enter to continue.");
+//Console.ReadLine();
+/**------------------------------------Stop Here-----------------------------------------**/
+
+/*
+
+Reasoning: Inserstion Sorting has a decent performance in comparison to Bubble, because of the
+model design. This algorithm performs well up to 100000 because it breaks the work apart more 
+efficiently. Essenstially it breaks the array into smaller groups and compares the numbers near 
+the break point to see if they are larger or smaller than the point in question. With in the 
+perameters of 100000 this algorithms quickly works through it causing it to run at a [O(nlogn)].
+
+ */
+
+// ----------------------------------------------------- Array Test
+//ArrayCheck(result);
+
+/**Other*/
+result = Reset;
+#endregion
+
+#region ** Run Merge Algorithm **
+/** Merge Sort Opperation **/
+//stopwatch.Reset();
+
+//Console.WriteLine("\nMerge Sorting Algorithm: ");
+//Console.WriteLine("Starting the sort.");
+
+//stopwatch.Start();
+
+//result = Sort.MergeSortAlgorithm(Mg, Mg.Length);
+//Console.WriteLine("Finished sorting!");
+
+//stopwatch.Stop();
+//DisplayRuntime(stopwatch);
+
+//Console.WriteLine("Press enter to continue.");
+//Console.ReadLine();
+/**------------------------------------Stop Here-----------------------------------------**/
+
+/*
+
+Reasoning: The Merger Sort was the best performing Algorithm, because off its effcient design,
+it breaks the array into two groups and filters then in order from smallest to largest number 
+value. Due to this precess it only really works through the array once to get to the final 
+ordered result. Within the parameters of 100000 it seemed to be working with [O(nlogn)].
+
+ */
+
+// ----------------------------------------------------- Array Test
+//ArrayCheck(result);
+
+/**Other*/
+result = Reset;
+#endregion
+
+#region ** Run Quick Sort Algorithm **
+/** Quick Sort Opperation **/
+stopwatch.Reset();
+
+Console.WriteLine("\nQuick Sorting Algorithm: ");
+Console.WriteLine("Starting the sort.");
+
+stopwatch.Start();
+
+Sort.QuickSort(Quick, 1, Quick.Length - 1);
+Console.WriteLine("Finished sorting!");
+
+stopwatch.Stop();
+DisplayRuntime(stopwatch);
+
+Console.WriteLine("Press enter to continue.");
+Console.ReadLine();
+/**------------------------------------Stop Here-----------------------------------------**/
+
+/*
+
+Reasoning: Like with the Insertion Sorting, I feel this one is a middle of the processing 
+Algorithms. I do however feel that this one would perform better with a higher amount needing 
+to be precessed. Because of the way this one is designed it works very sinmularly as the Merge.
+The only difference is the efficency of hw they operate because of how they are both desinged.
+Despite the difficulty I faced on getting this one to work with the others, I found this one 
+works with [O(nlogn)].
+
+ */
+
+// -----------------------------------------------------  Array Test
+ArrayCheck(Quick);
+#endregion
+
+#region ** Array Print Test **
+static void ArrayCheck(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.WriteLine(arr[i]);
+    }
+}
+#endregion
+
+#region ** Random Number Array Generator **
+static int[] GenerateRandomArray(int length, int minValue, int maxValue)
+{
+    Random rand = new Random();
+    int[] array = new int[length];
+
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = rand.Next(minValue, maxValue); // Generates a random integer within the specified range
+    }
+
+    return array;
+}
+#endregion
+
+#region ** Stop Watch **
+static void DisplayRuntime(Stopwatch stopwatch)
+{
+    TimeSpan ts = stopwatch.Elapsed;
+
+    // Format and display the TimeSpan value.
+    string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+        ts.Hours, ts.Minutes, ts.Seconds,
+        ts.Milliseconds / 10);
+    Console.WriteLine("Time Taken: " + elapsedTime);
+}
+#endregion
---
+++ Holloman_SortingAlgorithm_HW/Sort.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Holloman_SortingAlgorithm_HW
+{
+    public static class Sort
+    {
+        #region ** Bubble Sorting Algorithm **
+        public static int[] BubbleSort(int[] arr, int n)
+        {
+            int i, j, temp;
+            bool swapped;
+            for (i = 0; i < n - 1; i++)
+            {
+                swapped = false;
+                for (j = 0; j < n - i - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                        swapped = true;
+                    }
+                }
+            }
+            return arr;
+        }
+        #endregion
+
+        #region ** Insertion Sorting Algorithm **
+        public static int[] InsertionSort(int[] array, int length)
+        {
+            for (int i = 1; i < length; i++)
+            {
+                var key = array[i];
+                var flag = 0;
+                for (int j = i - 1; j >= 0 && flag != 1;)
+                {
+                    if (key < array[j])
+                    {
+                        array[j + 1] = array[j];
+                        j--;
+                        array[j + 1] = key;
+                    }
+                    else flag = 1;
+                }
+            }
+            return array;
+        }
+
+        //public static int[] InsertionSort(int[] array, int length)
+        //{
+        //    if (length <= 1)
+        //    {
+        //        return array;
+        //    }
+        //    InsertionSort(array, length - 1);
+        //    var key = array[length - 1];
+        //    var k = length - 2;
+        //    while (k >= 0 && array[k] > key)
+        //    {
+        //        array[k + 1] = array[k];
+        //        k = k - 1;
+        //    }
+        //    array[k + 1] = key;
+        //    return array;
+        //}
+        #endregion
+
+        #region ** Merge Sorting Algorithm **
+        public static int[] MergeSortAlgorithm(int[] arr, int n)
+        {
+            MergeSort(arr, 0, arr.Length - 1);
+            return arr;
+        }
+
+        private static void Merge(int[] arr, int l, int m, int r)
+        {
+            // Find sizes of two
+            // subarrays to be merged
+            int n1 = m - l + 1;
+            int n2 = r - m;
+
+            // Create temp arrays
+            int[] L = new int[n1];
+            int[] R = new int[n2];
+            int i, j;
+
+            // Copy data to temp arrays
+            for (i = 0; i < n1; ++i)
+                L[i] = arr[l + i];
+            for (j = 0; j < n2; ++j)
+                R[j] = arr[m + 1 + j];
+
+            // Merge the temp arrays
+
+            // Initial indexes of first
+            // and second subarrays
+            i = 0;
+            j = 0;
+
+            // Initial index of merged
+            // subarray array
+            int k = l;
+            while (i < n1 && j < n2)
+            {
+                if (L[i] <= R[j])
+                {
+                    arr[k] = L[i];
+                    i++;
+                }
+                else
+                {
+                    arr[k] = R[j];
+                    j++;
+                }
+                k++;
+            }
+
+            // Copy remaining elements
+            // of L[] if any
+            while (i < n1)
+            {
+                arr[k] = L[i];
+                i++;
+                k++;
+            }
+
+            // Copy remaining elements
+            // of R[] if any
+            while (j < n2)
+            {
+                arr[k] = R[j];
+                j++;
+                k++;
+            }
+        }
+
+        private static void MergeSort(int[] arr, int l, int r)
+        {
+            if (l < r)
+            {
+
+                // Find the middle point
+                int m = l + (r - l) / 2;
+
+                // QuickSort first and second halves
+                MergeSort(arr, l, m);
+                MergeSort(arr, m + 1, r);
+
+                // Merge the sorted halves
+                Merge(arr, l, m, r);
+            }
+        }
+        #endregion
+
+        #region ** Quick Sorting Algorithm **
+
+        // The QuickSort function implementation
+        public static void QuickSort(int[] arr, int left, int right)
+        {
+            if (left < right)
+            {
+                int pivot = Partition(arr, left, right);
+
+                QuickSort(arr, left, pivot - 1);
+                QuickSort(arr, pivot + 1, right);
+
+
+            }
+        }
+        private static int Partition(int[] arr, int left, int right)
+        {
+            int pivot = arr[right];
+            int i = left - 1;
+
+            for (int j = left; j < right; j++)
+            {
+                if (arr[j] <= pivot)
+                {
+                    i++;
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+
+            int temp1 = arr[i + 1];
+            arr[i + 1] = arr[right];
+            arr[right] = temp1;
+
+            return i + 1;
+        }
+
+
+
+        //public static int[] QuickSort(int[] array, int low, int high)
+        //{
+        //    int i = low;
+        //    int j = high;
+        //    int c = array[low];
+
+        //    while (i <= j)
+        //    {
+        //        while (array[i] < c)
+        //        {
+        //            i++;
+        //        }
+
+        //        while (array[j] > c)
+        //        {
+        //            j--;
+        //        }
+        //        if (i <= j)
+        //        {
+        //            int temp = array[i];
+        //            array[i] = array[j];
+        //            array[j] = temp;
+        //            i++;
+        //            j--;
+        //        }
+        //    }
+
+        //    if (low < j)
+        //    { QuickSort(array, low, j); }
+        //    if (i < high)
+        //    { QuickSort(array, i, high); }
+        //    return array;
+        //}
+
+        //public static void QuickSort(int[] arr, int low, int high)
+        //{
+        //    if (low < high)
+        //    {
+
+        //        // pi is the partition return index of c
+        //        int pi = Partition(arr, low, high);
+
+        //        // Recursion calls for smaller elements
+        //        // and greater or equals elements
+        //        QuickSort(arr, low, pi - 1);
+        //        QuickSort(arr, pi + 1, high);
+        //    }
+
+        //    // Swap function
+        //    static void Swap(int[] arr, int i, int j)
+        //    {
+        //        int temp = arr[i];
+        //        arr[i] = arr[j];
+        //        arr[j] = temp;
+        //    }
+
+        //    // Partition function
+        //    static int Partition(int[] arr, int low, int high)
+        //    {
+
+        //        // Choose the c
+        //        int c = arr[high];
+
+        //        // Index of smaller element and indicates 
+        //        // the right position of c found so far
+        //        int i = low - 1;
+
+        //        // Traverse arr[low..high] and move all smaller
+        //        // elements to the left side. Elements from low to 
+        //        // i are smaller after every iteration
+        //        for (int j = low; j <= high - 1; j++)
+        //        {
+        //            if (arr[j] < c)
+        //            {
+        //                i++;
+        //                Swap(arr, i, j);
+        //            }
+        //        }
+
+        //        // Move c after smaller elements and
+        //        // return its position
+        //        Swap(arr, i + 1, high);
+        //        return i + 1;
+        //    }
+
+    }
+        #endregion
+}
